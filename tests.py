8d3c3475ba00b7ea79f9827cf51c8c2ae6b63796
Леонид Куклин <kuklinlenya@mail.ru>
2026-02-21 06:50:02 +0700

    Добавление теста для тестирования приветствия

--- tests.py
+++ tests.py
@@ -8,5 +8,11 @@ class TestFriendCreation(unittest.TestCase):
         self.assertIsNotNone(friend)
 
 
+class TestGreeting(unittest.TestCase):
+    def test_greet_returns_string(self):
+        friend = ConsoleFriend()
+        self.assertEqual(friend.greet(), "я твой консольный друг, привет)")
+
+
 if __name__ == "__main__":
     unittest.main()

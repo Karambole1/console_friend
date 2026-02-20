5e391b5f5eb09f2f35062ca0062477307f12e34d
Леонид Куклин <kuklinlenya@mail.ru>
2026-02-21 06:59:43 +0700

    Добавление метода для установки имени

--- friend.py
+++ friend.py
@@ -1,7 +1,10 @@
 class ConsoleFriend:
     def __init__(self):
-        pass
+        self.name = None
 
     @staticmethod
     def greet():
         return "я твой консольный друг, привет)"
+
+    def set_name(self, name):
+        self.name = name

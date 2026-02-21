1aa0e65face91d13f7354f70fc41feeddd6cc1bb
Леонид Куклин <kuklinlenya@mail.ru>
2026-02-21 07:11:29 +0700

    Реализация ответа на вопрос содержащий 'как дела'

--- friend.py
+++ friend.py
@@ -8,3 +8,10 @@ class ConsoleFriend:
 
     def set_name(self, name):
         self.name = name
+
+    @staticmethod
+    def respond(message):
+        msg = message.lower()
+        if "как дела" in msg:
+            return "хорошо, спасибо, что спросил"
+        return "я пока не такой умный, чтобы отвечать на что-то кроме как 'как дела?'"
